32c07a76d8d0a31b709881159d69fe0101aeb147
Saad Qureshi <msaadq94@gmail.com>
2021-05-04 23:43:47 +0200

    Setup the project without Firebase

---
+++ Erun/Generated/Colors.swift
@@ -0,0 +1,15 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import SwiftUI
+
+public extension Color {
+  // Colors.xcassets
+  static var accentColor : Color { Color("AccentColor", bundle: BundleToken.bundle) }
+}
+
+private final class BundleToken {
+  static let bundle: Bundle = {
+    Bundle(for: BundleToken.self)
+  }()
+}
--- Pods/Target Support Files/Pods-Erun-ErunUITests/Pods-Erun-ErunUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Erun_ErunUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Erun_ErunUITests
-@end
