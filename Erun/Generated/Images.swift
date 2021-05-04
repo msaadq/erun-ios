d6f41098f13afa3e3b9f8708215a983a1f8e4a99
Saad Qureshi <msaadq94@gmail.com>
2021-05-05 01:50:18 +0200

    1-Project Setup (#3)
    - Setup the project 
    - Add Firebase dependencies

---
+++ Erun/Generated/Images.swift
@@ -0,0 +1,11 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import SwiftUI
+
+
+private final class BundleToken {
+  static let bundle: Bundle = {
+    Bundle(for: BundleToken.self)
+  }()
+}
