65a3936f23b4dc4692d645a0b741cf1481e14fab
Saad Qureshi <msaadq94@gmail.com>
2021-05-04 23:43:47 +0200

    Setup the project without Firebase

--- Erun/Components/ErunApp.swift
+++ Erun/Components/ErunApp.swift
@@ -2,18 +2,13 @@
 //  ErunApp.swift
 //  Erun
 //
-//  Created by Saad Qureshi on 27/04/2021.
+//  Created by Saad Qureshi on 04/05/2021.
 //
 
 import SwiftUI
-import Firebase
 
 @main
 struct ErunApp: App {
-    init() {
-        FirebaseApp.configure()
-    }
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
