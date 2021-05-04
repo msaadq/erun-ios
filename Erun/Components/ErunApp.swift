e1cdb8500bef5b4c9afcf697c974db4cd0da10d8
Saad Qureshi <msaadq94@gmail.com>
2021-05-05 00:09:10 +0200

    Add Firebase dependencies

--- Erun/Components/ErunApp.swift
+++ Erun/Components/ErunApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct ErunApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Setting up Firebase")
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
+
